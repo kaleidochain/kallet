3f96d457334446cdac0d3fb4991470b608f9b5e7
natrusio <natrusio@kaleidochain.io>
2019-09-24 14:43:00 +0800

    recover privacy routes

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
 importScripts(
-  "precache-manifest.f4cb8ca5d6729ccc5405d23a61c353f5.js"
+  "precache-manifest.3df3d970c0f6b39aaa9787afd7e44f25.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "myetherwallet"});
